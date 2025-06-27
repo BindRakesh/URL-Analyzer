15347c4a58239214ac33050eed9cf134944a65c7
BindRakesh <rks.bind@gmail.com>
2025-06-27 20:01:29 +0530

    Update URLAnalyzer.jsx
    small change in backend url

--- src/URLAnalyzer.jsx
+++ src/URLAnalyzer.jsx
@@ -42,7 +42,7 @@ const URLAnalyzer = () => {
     const urlList = urls.split('\n').filter(url => url.trim());
     setTotalUrls(urlList.length);
 
-     const backendUrl = 'wss://url-analyzer-backend-1-production.up.railway.app/test';   //railway anil
+     const backendUrl = 'wss://url-analyzer-backend-1-production.up.railway.app/analyze';   //railway anil
      //const backendUrl = 'wss://web-production-a69a9.up.railway.app/analyze'; //railway rakesh
     // const backendUrl = 'wss://url-analyzer-backend.onrender.com/analyze'; //render
     // const backendUrl = 'ws://localhost:8080/analyze'; //localhost
